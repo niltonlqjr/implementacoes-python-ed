edeef27637e843267846ffba90a108b575052115
niltonlqjr <niltonlqjr@gmail.com>
2023-11-14 02:00:13 -0300

    lista estatica correcoes

--- implementacoes_listas/lista_estatica.py
+++ implementacoes_listas/lista_estatica.py
@@ -13,28 +13,42 @@ class lista:
     __tam_max: int
     __elementos: list[item]
     __tam: int
+
     def __init__(self, tam_max: int):
         self.__tam = 0
         self.__tam_max = tam_max
-        self.__elementos = [int()] * tam_max
+        self.__elementos = [None] * tam_max
     
+    def vazia(self) -> bool:
+        return self.__tam == 0
+    
+    def cheia(self) -> bool:
+        return self.__tam == self.__tam_max
+
     def busca(self, ch: int) -> int:
         for i in range(self.__tam):
             if self.__elementos[i].chave == ch:
                 return i
         return -1
 
+    def busca_item(self, ch: int) -> item | None:
+        idx = self.busca(ch)
+        if idx != -1:
+            return deepcopy(self.__elementos[idx])
+        else:
+            return None
+
     def insere_fim(self, x: item) -> bool:
-        if (self.__tam < self.__tam_max) and (self.busca(x.chave) == -1):
+        if (not self.cheia()) and (self.busca(x.chave) == -1):
             self.__elementos[self.__tam] = deepcopy(x)
             self.__tam += 1
             return True
         return False
 
     def insere_pos(self, x: item, pos: int) -> bool:
-        if (self.__tam < self.__tam_max) and (pos <= self.__tam) and (self.busca(x.chave) == -1):
+        if (not self.cheia()) and (pos <= self.__tam) and (self.busca(x.chave) == -1):
             for i in range(self.__tam, pos, -1):
-                self.__elementos[i-1] = self.__elementos[i]
+                self.__elementos[i] = self.__elementos[i-1]
             self.__elementos[pos] = deepcopy(x)
             self.__tam += 1
             return True
@@ -46,13 +60,13 @@ class lista:
         
 
     def remove_fim(self) -> bool:
-        if (self.__tam > 0):
+        if not self.vazia():
             self.__tam -= 1
             return True
         return False
 
     def remove_pos(self, pos: int) -> bool:
-        if (self.__tam > 0):
+        if not self.vazia():
             self.__desloca(pos)
             return True
         return False
@@ -67,6 +81,7 @@ class lista:
     def string(self):
         l_str: str = '[ '
         for i in range(self.__tam):
+            print(i)
             l_str += '({ch}, {val}) '.format(
                 ch=self.__elementos[i].chave,
                 val=self.__elementos[i].valor)
---
+++ implementacoes_listas/teste_lista_estatica.py
@@ -0,0 +1,56 @@
+from lista_estatica import *
+
+l=lista(20)
+
+
+print('======INSERCAO=======')
+chaves=[1,3,5]
+for ch in chaves:
+    l.insere_pos(item(ch,0.0), 0)
+    print(l.string())
+
+chaves=[9,4,8,2]
+for ch in chaves:
+    l.insere_fim(item(ch,1.0))
+print(l.string())
+
+chavesp=[(7,0), (6,8)]
+for ch,p in chavesp:
+    l.insere_pos(item(ch,3.0),p)
+print(l.string())
+
+l.insere_fim(item(99,99))
+print(l.string())
+l.insere_pos(item(-1, -1), 0)
+print(l.string())
+
+print(l.string())
+print('======REMOCAO======')
+
+l.insere_fim(item(200,5.0))
+print(l.string())
+
+
+while not l.vazia():
+    l.remove_fim()
+    print(l.string())
+
+chaves=[12,3,8]
+for ch in chaves:
+    l.insere_fim(item(ch,7.0))
+print(l.string())
+
+print("=======BUSCA=======")
+
+it1 = l.busca_item(45)
+print(it1)
+
+it2 = l.busca_item(8)
+print(it2)
+
+it2.chave=99999999999
+print(l.string())
+
+chaves=[8872,8,3,12]
+print(l.string())
+
