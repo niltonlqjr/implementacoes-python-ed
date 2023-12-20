f0152cb3b1bc2321635e9ce49edb19ec3c003e10
niltonlqjr <niltonlqjr@gmail.com>
2023-12-20 01:50:27 -0300

    lista estatica cursor

--- implementacoes_listas/lista_estatica_cursor.py
+++ implementacoes_listas/lista_estatica_cursor.py
@@ -13,63 +13,135 @@ from copy import deepcopy
 
 @dataclass
 class item:
-    valor: int
+    chave: int
+    valor: float
+
     
 class no:
     def __init__(self, dado: item):
         self.dado = dado
-        self.prox = 0
+        self.prox = -1
     
 class lista:
     def __init__(self, tamanho:int):
         self.elementos: list[no | None] = [None] * tamanho
-        self.tamanho: int = tamanho
+        self.tamanho_maximo: int = tamanho
         self.primeiro: int = -1
         self.num_itens = 0
         self.disponiveis: fila = fila(tamanho+1)
         for i in range(tamanho):
-            self.disponiveis.enfileira(item(i))
+            self.disponiveis.enfileira(i)
         
     def vazia(self) -> bool:
         return self.num_itens == 0
     
     def cheia(self) -> bool:
-        return self.num_itens == self.tamanho
+        return self.disponiveis.vazia()
     
+    def busca(self, ch: int) -> int:
+        c: int = self.primeiro
+        while c != -1 and self.elementos[c].dado.chave != ch:
+            c = self.elementos[c].prox
+        return c
+
     def busca_item(self, ch: int) -> item | None:
-        pass
+        idx = self.busca(ch)
+        print(idx)
+        if idx != -1:
+            return deepcopy(self.elementos[idx].dado)
+        else:
+            return None
     
     def insere_ini(self, x:item) -> bool:
         if not self.cheia():
-            np = self.disponiveis.obtem_primeiro().valor
-            self.elementos[np] = no(deepcopy(x))
-            self.elementos[np].prox = self.primeiro
-            self.primeiro = np
+            pos: int = self.disponiveis.obtem_primeiro()
+            self.elementos[pos] = no(deepcopy(x))
+            self.elementos[pos].prox = self.primeiro
+            self.primeiro = pos
             self.num_itens += 1
             self.disponiveis.desenfileira()
             return True
         return False
 
+    def insere_fim(self, x:item) -> bool:
+        return self.insere_pos(x,self.num_itens)
+
     def insere_pos(self, x: item, pos: int) -> bool:
-        pass
-    
+        if not self.cheia():
+            if pos == 0:
+                return self.insere_ini(deepcopy(x))
+            else:
+                p: int = self.disponiveis.obtem_primeiro()
+                cur: int = self.primeiro
+                cont: int = 1
+                while cur != -1 and cont < pos:
+                    cur = self.elementos[cur].prox
+                    cont += 1
+                if cur != -1:
+                    self.elementos[p] = no(deepcopy(x))
+                    self.elementos[p].prox = self.elementos[cur].prox
+                    self.elementos[cur].prox = p
+                    self.num_itens += 1
+                    self.disponiveis.desenfileira()
+                    return True
+        return False
+
+        
     def remove_ini(self) -> bool:
         if not self.vazia():
             p = self.primeiro
             self.primeiro = self.elementos[p].prox
             self.num_itens -= 1
-            self.disponiveis.enfileira(item(p))
+            self.disponiveis.enfileira(p)
             return True
         return False
-        
-    def remove_chave(self, chave: int) -> bool:
-        pass
+
+    def remove_pos(self, pos: int) -> bool:
+        if not self.vazia():    
+            if pos == 0:
+                return self.remove_ini()
+            else:
+                cont: int = 0
+                cur: int = self.primeiro
+                while cur != -1 and cont < pos-1:
+                    cur = self.elementos[cur].prox
+                    cont += 1
+                if self.elementos[cur].prox != -1:
+                    d = self.elementos[cur].prox
+                    self.elementos[cur].prox = self.elementos[d].prox
+                    self.num_itens -= 1
+                    self.disponiveis.enfileira(d)
+                    return True
+        return False
+
+
+    def remove_chave(self, ch: int) -> bool:
+        if not self.vazia():
+            pos:int = self.primeiro
+            if self.elementos[pos].dado.chave == ch:
+                self.remove_ini()
+            else:
+                while pos != -1 and self.elementos[pos].dado.chave != ch:
+                    ant: int = pos
+                    pos = self.elementos[pos].prox
+                if pos != -1:
+                    self.elementos[ant].prox = self.elementos[pos].prox
+                    self.num_itens -= 1
+                    self.disponiveis.enfileira(pos)
+                    return True
+        return False
+
+    def remove_fim(self) -> bool:
+        if not self.vazia():
+            return self.remove_pos(self.num_itens-1)
+        return False
     
     def string(self) -> str:
-        x:int = self.primeiro
+        c:int = self.primeiro
         str='['
-        while x != -1:
-            str += '({0})'.format(self.elementos[x].dado.valor)
-            x = self.elementos[x].prox
+        while c != -1:
+            e: item = self.elementos[c].dado
+            str += '({0}, {1})'.format(e.chave, e.valor)
+            c = self.elementos[c].prox
         str+=']'
         return str
