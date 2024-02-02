4cfdf0e5373a89f83b4697710daf7d7b349ed411
niltonlqjr <niltonlqjr@gmail.com>
2024-02-02 17:09:19 -0300

    avl + teste

---
+++ arvores/avl/avl.py
@@ -0,0 +1,141 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from copy import deepcopy
+
+
+
+@dataclass
+class item:
+    chave: int
+    valor: float
+
+class no:
+    def __init__(self, dado: item):
+        self.esq: no | None = None
+        self.dir: no | None = None
+        self.altura: int = 1
+        self.dado: item = dado
+
+class arvore_avl:
+    def __init__(self):
+        self.raiz: no | None = None
+
+    def vazia(self):
+        return self.raiz == None
+
+    def insere(self, x:item):
+        self.raiz = self.insere_no(self.raiz, x)
+    
+    def busca(self, ch:int) -> item | None:
+        no = self.busca_no(self.raiz, ch)
+        if no != None:
+            return no.dado
+        else:
+            return None
+    
+    def remove(self, chave: int) -> bool:
+        self.raiz = self.remove_no(self.raiz, chave)
+
+    def busca_no(self, n: no, chave:int) -> no | None:
+        if n == None:
+            return None
+        elif chave > n.dado.chave:
+            return self.busca_no(n.dir, chave)
+        elif chave < n.dado.chave:
+            return self.busca_no(n.esq, chave)
+        else:
+            return n
+        
+    def insere_no(self, n: no , x: item) -> no | None:
+        if n == None:
+            n = no(deepcopy(x))
+        elif x.chave > n.dado.chave:
+            n.dir = self.insere_no(n.dir,x)
+        elif x.chave < n.dado.chave:
+            n.esq = self.insere_no(n.esq,x)
+        if n != None:
+            n.altura = self.altura_no(n)
+        return self.balanceia(n)
+
+    def troca_e_remove(self, no_atual: no, remover: no) -> no:
+        if no_atual.esq != None:
+            no_atual.esq = self.troca_e_remove(no_atual.esq, remover)
+        else:
+            remover.dado = no_atual.dado
+            remover = no_atual
+            no_atual = no_atual.dir
+        return self.balanceia(no_atual)
+    
+    def remove_no(self, n: no, chave: int) -> no | None:
+        if n != None:
+            if n.dado.chave < chave:
+                n.dir = self.remove_no(n.dir, chave)
+            elif n.dado.chave > chave:
+                n.esq = self.remove_no(n.esq, chave)
+            else:
+                if n.esq != None and n.dir != None:
+                    n.dir = self.troca_e_remove(n.dir, n)
+                if n.esq == None:
+                    n = n.dir
+                else:
+                    n = n.dir
+        return self.balanceia(n)
+    
+    def altura_no(self, n:no) -> int:
+        altura_sad = 0
+        altura_sae = 0
+        altura_no = 0
+        if n != None:
+            if n.dir != None:
+                altura_sad = n.dir.altura
+            if n.esq != None:
+                altura_sae = n.esq.altura
+            altura_no = max(altura_sae,altura_sad) + 1
+        return altura_no
+    
+    def rotacao_esq(self, p: no) -> no:
+        q = p.dir
+        p.dir = q.esq
+        q.esq = p
+        p.altura = self.altura_no(p)
+        q.altura = self.altura_no(q)
+        return q
+    
+    def rotacao_dir(self, p: no) -> no:
+        q = p.esq
+        p.esq = q.dir
+        q.dir = p
+        p.altura = self.altura_no(p)
+        q.altura = self.altura_no(q)
+        return q
+    
+    def balanceia(self, n: no) -> no:
+        if n!= None:
+            asd = self.altura_no(n.dir)
+            ase = self.altura_no(n.esq)
+            if asd > ase + 1 :
+                if self.altura_no(n.dir.esq) > self.altura_no(n.dir.dir):
+                    n.dir = self.rotacao_dir(n.dir)
+                return self.rotacao_esq(n)
+            elif ase > asd + 1:
+                if self.altura_no(n.esq.dir) > self.altura_no(n.esq.esq):
+                    n.esq = self.rotacao_esq(n.esq)
+                return self.rotacao_dir(n)
+        return n
+    
+    def string_ramo(self, n:no, nivel:int):
+        s = ''
+        s += '  '*nivel
+        s += '|____'
+        if n != None:
+            s += str(n.dado.chave) +' - '+ str(n.altura) +'\n'
+        else:
+            s += '\n'
+        if n != None:
+            str_esq = self.string_ramo(n.esq,nivel+1)
+            str_dir = self.string_ramo(n.dir,nivel+1)
+            s += str_esq + str_dir
+        return s
+
+    def string(self):
+        return self.string_ramo(self.raiz, 0)
---
+++ arvores/avl/teste_avl.py
@@ -0,0 +1,44 @@
+from avl import *
+
+ 
+a1=arvore_avl()
+a2=arvore_avl()
+dados_insere=[1,12,10, 29, 36, 5, 33, 27, 40, 4, 7, 20, 23, 35, 37,42,21,22,23]
+dados_busca=[12,10, 22, 36, 5, 33, 27, 40, 4, 7, 20, 23, 35, 37,42,24]
+dados_remove=[12,10, 22, 36, 5, 33, 27, 40, 4, 7, 20, 23, 35, 37,42,24,1,29]
+#dados = [5,3,9,2,4,7,10,1,2.5,3.5,4.5,6,8,9.5,11]
+#dados=[19,2,3,4,5,6,7,8,9,10,11,12,13,20]
+val=''
+
+dados = dados_insere
+
+for ch in dados:
+    x=item(ch,val)
+    a1.insere(x)
+    x=item(ch,val)
+    a2.insere(x)
+    print('===inseriu {0}==='.format(ch))
+    print(a1.string())
+    print('=================')
+
+dados = dados_busca
+
+for ch in dados:
+    x=a1.busca(ch)
+    print(x)
+
+dados = dados_remove
+
+for ch in dados:
+    a2.remove(ch)
+    print('====removeu {0}===='.format(ch))
+    print(a2.string())
+    print('===================')
+
+
+       
+        
+
+
+
+
